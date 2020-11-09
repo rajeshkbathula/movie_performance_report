28290a0427496c92a2b2a3207abf23ef24076e3e
Rajesh K Bathula <33092055+rajeshkbathula@users.noreply.github.com>
2020-11-09 21:35:29 +0000

    infrastructure and postgres settign with connection testing in docker (#6)
    * infrastructure and postgres settign with connection testing in docker
    
    * main file time num functiona  and db credentials correction with unit test connection test
    
    * grafana link in readme

---
+++ script/postgres_feed_data.py
@@ -0,0 +1,54 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+from config import *
+
+def postgres_connect(user='db_admin',passwd='db_admin',db='movie_database'):
+    """
+    postgres connection using psycopg2 module
+    :param user: str :usename
+            passwd :  str : password
+            db : str : database
+    :return: Bool true or false
+    """
+    try:
+        alchemyEngine = create_engine(f'postgresql+psycopg2://{user}:{passwd}@localhost/{db}',
+                                      pool_recycle=3600)
+        postgreSQLConnection = alchemyEngine.connect()
+    except Exception as e:
+        logger.error(f"Postgres connection Failed to DB {db} with exception {e}")
+    else:
+        logger.info(f"Postgres DB connection successful to DB :  {db} has been created")
+        return postgreSQLConnection
+
+def pandas_to_postgres_table(table,pandas_df,connection=None):
+    """
+    this function will inject data from Pandas DF into Postgres
+    :param connection: connection :postgres connection
+            table :  str : table name
+            pandas_df : DataFrame
+    :return: Bool true or false
+    """
+    if not connection:
+        connection = postgres_connect()
+    try:
+        pandas_df.to_sql(table, connection)
+    except ValueError as vx:
+        logger.error(f"Postgres connection Failed to ValueError with exception  {vx}")
+    except Exception as ex:
+        logger.error(f"Postgres connection Failed With Exception {ex}")
+    else:
+        logger.info(f"Postgres Table {table} has been created successfully.")
+    finally:
+        connection.close()
+        return True
+
+def postgres_view_with_final_results(connection, table,pandas_df):
+    """
+    this function will inject data from Pandas DF into Postgres
+    :param connection: connection :postgres connection
+            table :  str : table name
+            pandas_df : DataFrame
+    :return: Bool true or false
+    """
+    pass
---
+++ script/test/test_db_connect_tablesload_test.py
@@ -0,0 +1,44 @@
+"""
+unittest framework to test Postgres connection and data load
+"""
+import unittest
+import uuid
+from main import *
+
+class TestmMainScript(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.thisdir = os.path.dirname(os.path.abspath(__file__))
+        movies_metadata_csv_path = os.path.join(cls.thisdir, './resources/movie_csv_df.csv')
+        cls.movies_metadata_df = pd.read_csv(movies_metadata_csv_path, delimiter="§", engine='python')
+
+    def test_postgres_connect(self):
+        '''
+        This function will test Postgres DB Connection
+        '''
+        try:
+            connect = postgres_connect('test_user', 'psswd', 'test_db')
+            result = connect.engine
+            connect.close()
+        except Exception as e:
+            result = None
+        expected = 'Engine(postgresql+psycopg2://test_user:***@localhost/test_db)'
+        self.assertEqual(expected, str(result), "Pastgres DB function failed!")
+
+    def test_postgres_feed(self):
+        '''
+        This function will test df load to pandas dataframe can be extended reading and texting data.
+        '''
+        test_table = f"test_{uuid.uuid1()}"
+        connection = postgres_connect('test_user', 'psswd', 'test_db')
+        inject_data = pandas_to_postgres_table(test_table,self.movies_metadata_df,connection)
+        self.assertTrue(inject_data,"Pd to Postgres inject failed!")
+
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
