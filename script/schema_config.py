758c3e7d34e3c55cc667eb787490ac007727840d
Rajesh Bathula <kumar.bathula19@gmail.com>
2020-11-09 14:58:12 +0000

    schema config cleanup

--- script/schema_config.py
+++ script/schema_config.py
@@ -1,13 +1,7 @@
-raw_xml_root = ['feed',  'doc']
-
-raw_xml_flatten_columns = ['title', 'url']
 
 wiki_df_columns = ['title','link','abstract']
 
 raw_csv_columns = ['title','budget','release_date','revenue','vote_average','vote_average','production_companies']
 
-
 csv_filtered_columns = {'budget':'int','genres':'string','title':'string','companies':'string',
                                   'release_date':'string','revenue':'int','rating':'float'}
-
-csv_columns = ['title','budget','year','revenue','budget','rating','ratio','production_company','wiki_link','wiki_budget']
